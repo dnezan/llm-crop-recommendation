87f81f95252cf22748cc5748ad5c6a2d2fe4a5aa
dineshsreekanthan <dinsree11@gmail.com>
2023-05-25 21:25:09 +0530

    Updated functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,22 @@ import folium
 import streamlit as st
 import geemap.colormaps as cm
 import geemap.foliumap as geemap
+import datetime
 from datetime import date
+from io import BytesIO
+buf = BytesIO(b'test')
+
+def save_uploadedfile(uploadedfile):
+     with open(os.path.join("tempDir",uploadedfile.name),"wb") as f:
+         f.write(uploadedfile.getbuffer())
 
 def app():
     st. set_page_config(layout="wide")
-    st.title(":calendar: Crop Planner App")
+    st.title(" ")
     
     st.markdown("""
-        <style>
+        <style>        
+        #MainMenu {visibility: hidden;}
                .block-container {
                     padding-top: 1rem;
                     padding-bottom: 0rem;
@@ -23,49 +31,65 @@ def app():
                 }
         </style>
         """, unsafe_allow_html=True)
-    
-    if st.session_state.get("zoom_level") is None:
-        st.session_state["zoom_level"] = 4
 
-    st.session_state["ee_asset_id"] = None
-    st.session_state["bands"] = None
-    st.session_state["palette"] = None
-    st.session_state["vis_params"] = None
-
-    row1_col1, row1_col2 = st.columns([1, 1])
+    row1_col1, row1_col2 = st.columns([3, 2])
 
     with row1_col1:
-        row2_col1, row2_col2 = st.columns([1, 1])
-        with row2_col1:
-            keyword = st.text_input("Search for a location:", "")   
-        with row2_col2:
-            w = st.file_uploader("Upload a GeoJSON file", type="geojson")
         m = geemap.Map(
         center=[20,77], 
         zoom=4,
-        basemap="HYBRID",
+        basemap="SATELLITE",
         plugin_Draw=True,
         Draw_export=True,
         locate_control=False,
-        plugin_LatLngPopup=False)
-        m.to_streamlit(width=1300, height=360)
-        if keyword:
-            locations = geemap.geocode(keyword)
-            if locations is not None and len(locations) > 0:
-                str_locations = [str(g)[1:-1] for g in locations]
-                location = st.selectbox("Select a location:", str_locations)
-                #st.write(str_locations[0])
-                loc_index = str_locations.index(location)
-                selected_loc = locations[loc_index]
-                lat, lng = selected_loc.lat, selected_loc.lng
-                print(lat)
-                print(lng)
-                folium.Marker(location=[lat, lng], popup=location).add_to(m)
-                m.set_center(lng, lat, 12)
-                st.session_state["zoom_level"] = 12
+        plugin_LatLngPopup=False, 
+        measure=True)    
+        m.to_streamlit(width=700, height=670)
+ 
+    
+        
 
     with row1_col2:
-        st.write("hi")
+        with st.form("my_form"):
+            st.header(":calendar: Crop Planning App")
+            w = st.file_uploader("", type="geojson")
+            if w is not None:
+                save_uploadedfile(w)
+                st.write("chung")
+
+                df = gpd.read_file("./tempDir/" + w.name)
+
+            #Find the center point
+                df['Center_point'] = df['geometry'].centroid
+            #Extract lat and lon from the centerpoint
+                df["lat"] = df.Center_point.map(lambda p: p.x)
+                df["long"] = df.Center_point.map(lambda p: p.y)
+                uploaded_lat_val = (df["lat"][0])
+                uploaded_long_val = (df["long"][0])
+                st.write(uploaded_lat_val)
+                st.write(uploaded_long_val)
+                ee_object = geemap.geojson_to_ee("./tempDir/" + w.name)
+                m.addLayer(ee_object, {}, 'Layer name')
+                m.centerObject(ee_object, 19)
+
+
+            #folium_map = folium.Map(location=[uploaded_long_val, uploaded_lat_val], zoom_start=17, width=1000, height=350, tiles=tile_url, attr='Tiles &copy; Esri', control_scale=True)
+           
+
+            d = st.date_input("When do you want to plant your crop?")
+        
+        
+   # Every form must have a submit button.
+            submitted = st.form_submit_button("Submit")
+            if submitted:
+                func(uploaded_long_val, uploaded_lat_val,d)
+        
+        row2_col1, row2_col2 = st.columns([1, 1])
+     
+        #swith row2_col2:
+
+def func(uploaded_long_val, uploaded_lat_val,d):
+    st.write(d)      
 
     
 
